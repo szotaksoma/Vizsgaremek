a601e02568194d53a235f491f7a8da0ec2de1296
szotaksimon <szotak.simon16@gmail.com>
2022-03-07 20:41:21 +0100

    adatbázis frissítve

--- project.sql
+++ project.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.7
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Mar 02, 2022 at 11:29 AM
--- Server version: 8.0.28
--- PHP Version: 7.3.33
+-- Host: 127.0.0.1
+-- Generation Time: Mar 07, 2022 at 08:36 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,10 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `salamonb_python-login`
+-- Database: `project`
 --
-CREATE DATABASE IF NOT EXISTS `salamonb_python-login` DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
-USE `salamonb_python-login`;
+CREATE DATABASE IF NOT EXISTS `project` DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
+USE `project`;
 
 -- --------------------------------------------------------
 
@@ -31,8 +30,8 @@ USE `salamonb_python-login`;
 --
 
 CREATE TABLE `category` (
-  `id` int NOT NULL,
-  `category` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci NOT NULL
+  `id` int(11) NOT NULL,
+  `category` varchar(45) COLLATE utf8mb4_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
@@ -53,9 +52,9 @@ INSERT INTO `category` (`id`, `category`) VALUES
 --
 
 CREATE TABLE `session` (
-  `id` int NOT NULL,
-  `user_id` int NOT NULL,
-  `token_hash` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci NOT NULL
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `token_hash` varchar(64) COLLATE utf8mb4_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
@@ -72,13 +71,13 @@ INSERT INTO `session` (`id`, `user_id`, `token_hash`) VALUES
 --
 
 CREATE TABLE `todo` (
-  `id` int NOT NULL,
-  `user_id` int NOT NULL,
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `todo` varchar(256) COLLATE utf8_hungarian_ci NOT NULL,
-  `category_id` int NOT NULL,
-  `creation_date` int NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `creation_date` int(11) NOT NULL,
   `done` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_hungarian_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 -- --------------------------------------------------------
 
@@ -87,16 +86,15 @@ CREATE TABLE `todo` (
 --
 
 CREATE TABLE `user` (
-  `id` int NOT NULL,
-  `username` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `password_hash` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `password_salt` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `email` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `first_name` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `last_name` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `gender` int NOT NULL,
-  `register_date` bigint NOT NULL,
-  `password_reset_token` char(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci DEFAULT NULL,
+  `id` int(11) NOT NULL,
+  `username` varchar(45) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `password_hash` varchar(64) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `password_salt` varchar(64) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `email` varchar(45) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `first_name` varchar(45) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `last_name` varchar(45) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `register_date` bigint(20) NOT NULL,
+  `password_reset_token` char(64) COLLATE utf8mb4_hungarian_ci DEFAULT NULL,
   `email_verification_token` char(64) COLLATE utf8mb4_hungarian_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
@@ -104,8 +102,8 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password_hash`, `password_salt`, `email`, `first_name`, `last_name`, `gender`, `register_date`, `password_reset_token`, `email_verification_token`) VALUES
-(31, 'admin', '3efe0ecc33bcf6dfeaac9021f87594ec517db8eb43fee3e215f896694f528780', '7206cbe8399e3061c5132b3ad817eb30a5b401f06a87f37c9af00b5b59817213', 'admin@example.com', 'Project', 'Admin', 0, 1645793563, NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password_hash`, `password_salt`, `email`, `first_name`, `last_name`, `register_date`, `password_reset_token`, `email_verification_token`) VALUES
+(31, 'admin', '3efe0ecc33bcf6dfeaac9021f87594ec517db8eb43fee3e215f896694f528780', '7206cbe8399e3061c5132b3ad817eb30a5b401f06a87f37c9af00b5b59817213', 'admin@example.com', 'Project', 'Admin', 1645793563, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -146,25 +144,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `session`
 --
 ALTER TABLE `session`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
 
 --
 -- AUTO_INCREMENT for table `todo`
 --
 ALTER TABLE `todo`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- Constraints for dumped tables
